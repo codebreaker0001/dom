b5dcee37ea5b79c11ba0a0d39cf25b84a022d961
codebreaker0001 <122373416+codebreaker0001@users.noreply.github.com>
2023-03-11 17:21:50 +0530

    Delete ass1.js

--- ass1.js
+++
@@ -1,9 +0,0 @@
-let a= document.getElementsByClassName("dec");
-let b= document.getElementsByClassName("inc");
-let c= document.getElementsByClassName("input");
-console.log(c);
-
-function decrease(){
-    
-    
-}
