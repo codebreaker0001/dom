6ddb6e25b196f1927cc6a4802e1da7f7307cda79
codebreaker0001 <122373416+codebreaker0001@users.noreply.github.com>
2023-03-11 17:20:59 +0530

    Add files via upload

---
+++ ass1.js
@@ -0,0 +1,9 @@
+let a= document.getElementsByClassName("dec");
+let b= document.getElementsByClassName("inc");
+let c= document.getElementsByClassName("input");
+console.log(c);
+
+function decrease(){
+    
+    
+}
